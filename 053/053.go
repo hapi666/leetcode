cfe5f692941f36214cdb2c16cf74dbeed42e69d7
hapi666 <281127500@qq.com>
2019-04-09 19:17:50 +0800

    三种求最大连续子序列算法

--- 053/053.go
+++ 053/053.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // O(n^2)解法
-func maxSubArray(nums []int) int {
+func maxSubArray1(nums []int) int {
 	sum := 0
 	max := nums[0]
 	for i := range nums {
@@ -18,7 +18,58 @@ func maxSubArray(nums []int) int {
 	return max
 }
 
+// O(nlgn) 分治法 时间复杂度最小
+func maxSubArray2(nums []int) int {
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	middle := len(nums) >> 1
+	mleft := maxSubArray2(nums[:middle])
+	mright := maxSubArray2(nums[middle:])
+	maxl := nums[middle-1]
+	suml := 0
+	for index := middle - 1; index >= 0; index-- {
+		suml += nums[index]
+		if suml > maxl {
+			maxl = suml
+		}
+	}
+	maxr := nums[middle]
+	sumr := 0
+	for index := middle; index < len(nums); index++ {
+		sumr += nums[index]
+		if sumr > maxr {
+			maxr = sumr
+		}
+	}
+	max := maxl + maxr
+	if max < mleft {
+		max = mleft
+	}
+	if max < mright {
+		max = mright
+	}
+	return max
+}
+
+// O(n) 动态规划算法
+func maxSubArray3(nums []int) int {
+	sum := nums[0]
+	b := 0
+	for index := 0; index < len(nums); index++ {
+		if b > 0 {
+			b += nums[index]
+		} else {
+			b = nums[index]
+		}
+		if b > sum {
+			sum = b
+		}
+	}
+	return sum
+}
+
 func main() {
 	nums := []int{-2, -1}
-	fmt.Println(maxSubArray(nums))
+	fmt.Println(maxSubArray3(nums))
 }
